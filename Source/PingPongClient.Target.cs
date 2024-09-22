a0ed0e69c506635c020cbaf45d1b636c34955fc0
Makyrios <43245644+Makyrios@users.noreply.github.com>
2024-09-23 00:48:21 +0300

    Update PingPongClient.Target.cs

--- Source/PingPongClient.Target.cs
+++ Source/PingPongClient.Target.cs
@@ -9,8 +9,8 @@ public class PingPongClientTarget : TargetRules
 	public PingPongClientTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Client;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.AddRange(new string[] { "PingPong" });
 	}
 }
